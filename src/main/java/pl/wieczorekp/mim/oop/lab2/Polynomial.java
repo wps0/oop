5d2d0684dd3342f01f38f6eeff0290bc1141008f
wps0 <53907986+wps0@users.noreply.github.com>
2023-07-01 19:57:08 +0200

    wip on gates

--- src/main/java/pl/wieczorekp/mim/oop/lab2/Polynomial.java
+++
@@ -1,4 +0,0 @@
-package pl.wieczorekp.mim.oop.lab2;
-
-public class Polynomial {
-}
--- src/main/java/pl/wieczorekp/mim/oop/lab9/gates/NOTGate.java
+++ src/main/java/pl/wieczorekp/mim/oop/lab9/gates/NOTGate.java
@@ -1,11 +1,11 @@
-package pl.wieczorekp.mim.oop.lab9.gates;
-
-public class NOTGate extends SingleInputSingleOutputGate {
-    @Override
-    public boolean evaluate() {
-        if (!isReadyToBeEvaluated()) {
-            throw new IllegalStateException();
-        }
-        return this.activeInputs == 0;
-    }
-}
+//package pl.wieczorekp.mim.oop.lab9.gates;
+//
+//public class NOTGate extends SingleInputSingleOutputGate {
+//    @Override
+//    public boolean evaluate() {
+//        if (!isReadyToBeEvaluated()) {
+//            throw new IllegalStateException();
+//        }
+//        return this.activeInputs == 0;
+//    }
+//}
