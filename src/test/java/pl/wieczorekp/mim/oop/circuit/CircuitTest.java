9f5a10b79e6b509599488e2c9ced69aea5251d9b
wps0 <53907986+wps0@users.noreply.github.com>
2023-07-10 22:09:05 +0200

    Add more circuit tests

--- src/test/java/pl/wieczorekp/mim/oop/circuit/CircuitTest.java
+++ src/test/java/pl/wieczorekp/mim/oop/circuit/CircuitTest.java
@@ -25,4 +25,44 @@ class CircuitTest {
         assertEquals(3, actualLength);
         assertEquals(7, actualSize);
     }
+
+    @ParameterizedTest
+    @MethodSource("pl.wieczorekp.mim.oop.circuit.gates.GateTestUtils#primitive3BooleanArgumentSource")
+    void givenSampleXorCircuitShouldEvaluateToCorrectValues(boolean[] inputs) {
+        // given
+        Circuit sampleCircuit = CircuitBuilder.buildSampleXorCircuit();
+        boolean expected = (inputs[0] ^ inputs[1]) | inputs[2];
+
+        // when
+        boolean actualValue = sampleCircuit.evaluate(inputs);
+        int actualDepth = sampleCircuit.depth();
+        int actualLength = sampleCircuit.inputLength();
+        int actualSize = sampleCircuit.size();
+
+        // then
+        assertEquals(expected, actualValue);
+        assertEquals(3, actualDepth);
+        assertEquals(3, actualLength);
+        assertEquals(7, actualSize);
+    }
+
+    @ParameterizedTest
+    @MethodSource("pl.wieczorekp.mim.oop.circuit.gates.GateTestUtils#primitive3BooleanArgumentSource")
+    void givenSampleCircuitWithMajorityGateShouldEvaluateToCorrectValues(boolean[] inputs) {
+        // given
+        Circuit sampleCircuit = CircuitBuilder.buildSampleMajorityCircuit();
+        boolean expected = ((inputs[0] ^ inputs[1]) | inputs[2]) & inputs[1];
+
+        // when
+        boolean actualValue = sampleCircuit.evaluate(inputs);
+        int actualDepth = sampleCircuit.depth();
+        int actualLength = sampleCircuit.inputLength();
+        int actualSize = sampleCircuit.size();
+
+        // then
+        assertEquals(expected, actualValue);
+        assertEquals(2, actualDepth);
+        assertEquals(3, actualLength);
+        assertEquals(8, actualSize);
+    }
 }
---
+++ src/test/java/pl/wieczorekp/mim/oop/circuit/gates/MajorityGateTest.java
@@ -0,0 +1,49 @@
+package pl.wieczorekp.mim.oop.circuit.gates;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import pl.wieczorekp.mim.oop.circuit.Circuit;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MajorityGateTest {
+    @ParameterizedTest
+    @MethodSource("pl.wieczorekp.mim.oop.circuit.gates.GateTestUtils#primitiveBooleanArgumentSource")
+    void givenNInputsShouldMajorityCorrectly(boolean[] inputs) {
+        // given
+        Boolean[] nonPrimitiveInputs = new Boolean[inputs.length];
+        IntStream.range(0, inputs.length).forEach(i -> nonPrimitiveInputs[i] = inputs[i]);
+        long trueCount = Arrays.stream(nonPrimitiveInputs).filter(val -> val).count();
+        boolean expected = trueCount*2 > inputs.length;
+
+        Circuit c = new Circuit();
+        Gate gate = new MajorityGate();
+        TerminatingGate sink = new TerminatingGate();
+        List<SignalEmittingGate> taps = IntStream.range(0, inputs.length)
+                .mapToObj(i -> new SignalEmittingGate(inputs[i]))
+                .collect(Collectors.toList());
+
+        for (Gate tap : taps) {
+            Wire tapGateWire = new Wire(tap, gate);
+            tap.connectTo(tapGateWire);
+            gate.connectTo(tapGateWire);
+        }
+        Wire gateSinkWire = new Wire(gate, sink);
+        gate.connectTo(gateSinkWire);
+        sink.connectTo(gateSinkWire);
+
+        c.addInputGates(taps);
+        c.setOutputGate(sink);
+
+        // when
+        boolean actual = c.evaluate(inputs);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
